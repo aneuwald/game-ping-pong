201d51714c34169f98c9ec18edbe0dcbdb9570c4
Alexandre Neuwald <alexandre.neuwald@criticaltechworks.com>
2025-02-25 21:14:34 +0000

    create class ball

---
+++ scripts/ball.js
@@ -0,0 +1,42 @@
+class Ball {
+    x;
+    y;
+    size;
+    velocity;
+    velX;
+    velY;
+
+    constructor(x, y, size, vel) {
+        this.x = x;
+        this.y = y;
+
+        this.size = size;
+        this.velocity = vel;
+
+        this.velX = vel;
+        this.velY = vel;
+    }
+
+    update() {
+        
+        if (this.x > WIDTH - this.size / 2) {
+            this.velX = this.velocity * -1;
+        }
+        if (this.x < this.size / 2) {
+            this.velX = this.velocity;
+        }
+
+        this.x += this.velX;
+        
+        if (this.y > HEIGHT - this.size / 2) {
+            this.velY = this.velocity * -1;
+        }
+        if (this.y < this.size / 2) {
+            this.velY = this.velocity;
+        }
+
+        this.y += this.velY;
+
+        ellipse(this.y, this.x, this.size);
+    }
+}
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,1 +1,19 @@
-alert('works')
+const WIDTH = 500;
+const HEIGHT = 800;
+
+let ball;
+let ball2;
+
+function setup() {
+    createCanvas(HEIGHT, WIDTH);
+
+    ball = new Ball(100, 100, 50, 5);
+    ball2 = new Ball(100, 700, 20, 10);
+}
+
+function draw() {
+    background(200);
+    ball.update();
+    ball2.update();
+}
+
